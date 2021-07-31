7d5b77efb7a355f66958315442d432c986e34547
DATA SCIENCE <bryanbuitrago86@gmail.com>
2021-07-31 00:10:55 -0400

    Initial set up

--- hungry-pets/frontend/src/components/PetList.js
+++ hungry-pets/frontend/src/components/PetList.js
@@ -1,11 +1,27 @@
 import React from "react";
+import { Row, Col } from "react-bootstrap";
 import PetCard from "./PetCard";
+
+import pets from "../pets";
 
 const PetList = () => {
   return (
     <div>
+      <h1>Pet List</h1>
       <ul>
-        <PetCard />
+        <Row>
+          {pets.map((pet) => (
+            <Col>
+              <PetCard
+                name={pet.name}
+                species={pet.species}
+                birthYear={pet.birthYear}
+                img={pet.photo}
+                favFoods={pet.favFoods}
+              />
+            </Col>
+          ))}
+        </Row>
       </ul>
     </div>
   );
