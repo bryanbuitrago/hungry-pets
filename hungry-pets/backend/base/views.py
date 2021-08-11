cc9b3fcdd44e7b3476e3dba573c14eb02a96b735
DATA SCIENCE <bryanbuitrago86@gmail.com>
2021-08-10 23:59:43 -0400

    Installed django rest framework

--- hungry-pets/backend/base/views.py
+++ hungry-pets/backend/base/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from .pets import pets
 
 # Create your views here.
 
 
+@api_view(['GET'])
 def getRoutes(request):
     routes = [
         '/api/pets/',
@@ -12,7 +15,7 @@ def getRoutes(request):
         '/api/pets/update/<id>',
         '/api/pets/delete/<id>',
     ]
-    return JsonResponse(routes, safe=False)
+    return Response(routes)
 
 
 def getPets(request):
