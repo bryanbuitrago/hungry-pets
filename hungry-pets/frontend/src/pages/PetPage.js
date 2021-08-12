689e7c5d9c6d5024cda68e5caef9fc909946bdb1
DATA SCIENCE <bryanbuitrago86@gmail.com>
2021-08-12 16:26:40 -0400

    Add API call to fetch one pet by id

--- hungry-pets/frontend/src/pages/PetPage.js
+++ hungry-pets/frontend/src/pages/PetPage.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
-import pets from "../components/PetList";
+import PetCard from "../components/PetCard";
+// import pets from "../components/PetList";
+import axios from "axios";
 
 const PetPage = ({ match }) => {
-  //   const pet = pets.find((p) => p.birthYear == match.params.id);
-  return <div>Pet Page</div>;
+  const [pet, setPet] = useState([]);
+  console.log(pet);
+
+  useEffect(() => {
+    async function fetchPet() {
+      const { data } = await axios.get(`/api/pets/${match.params.id}`);
+      setPet(data);
+    }
+
+    fetchPet();
+  }, []);
+
+  return (
+    <div>
+      <Link to="/" className="btn btn my-3">
+        Go Back
+      </Link>
+    </div>
+  );
 };
 
 export default PetPage;
