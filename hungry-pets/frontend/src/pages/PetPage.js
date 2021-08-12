65ed601434617aee13c2cca897c2b6b34ddff2c1
DATA SCIENCE <bryanbuitrago86@gmail.com>
2021-08-12 14:33:19 -0400

    Add Pet Page Route

---
+++ hungry-pets/frontend/src/pages/PetPage.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import { Row, Col, Image, ListGroup, Button, Card } from "react-bootstrap";
+import pets from "../components/PetList";
+
+const PetPage = ({ match }) => {
+  //   const pet = pets.find((p) => p.birthYear == match.params.id);
+  return <div>Pet Page</div>;
+};
+
+export default PetPage;
